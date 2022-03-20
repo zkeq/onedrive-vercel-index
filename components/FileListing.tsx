05ba12d8756078bf1233311ed6e0f45ed8df7a70
Zkeq <learnonly@learnonly.xyz>
2022-03-20 09:57:52 +0800

    Update FileListing.tsx

--- components/FileListing.tsx
+++ components/FileListing.tsx
@@ -89,50 +89,50 @@ export const ChildIcon: FC<{ child: OdFolderChildren }> = ({ child }) => {
   )
 }
 
-export const Checkbox: FC<{
-  checked: 0 | 1 | 2
-  onChange: () => void
-  title: string
-  indeterminate?: boolean
-}> = ({ checked, onChange, title, indeterminate }) => {
-  const ref = useRef<HTMLInputElement>(null)
+// export const Checkbox: FC<{
+//   checked: 0 | 1 | 2
+//   onChange: () => void
+//   title: string
+//   indeterminate?: boolean
+// }> = ({ checked, onChange, title, indeterminate }) => {
+//   const ref = useRef<HTMLInputElement>(null)
 
-  useEffect(() => {
-    if (ref.current) {
-      ref.current.checked = Boolean(checked)
-      if (indeterminate) {
-        ref.current.indeterminate = checked == 1
-      }
-    }
-  }, [ref, checked, indeterminate])
+//   useEffect(() => {
+//     if (ref.current) {
+//       ref.current.checked = Boolean(checked)
+//       if (indeterminate) {
+//         ref.current.indeterminate = checked == 1
+//       }
+//     }
+//   }, [ref, checked, indeterminate])
 
-  const handleClick: MouseEventHandler = e => {
-    if (ref.current) {
-      if (e.target === ref.current) {
-        e.stopPropagation()
-      } else {
-        ref.current.click()
-      }
-    }
-  }
+//   const handleClick: MouseEventHandler = e => {
+//     if (ref.current) {
+//       if (e.target === ref.current) {
+//         e.stopPropagation()
+//       } else {
+//         ref.current.click()
+//       }
+//     }
+//   }
 
-  return (
-    <span
-      title={title}
-      className="inline-flex cursor-pointer items-center rounded p-1.5 hover:bg-gray-300 dark:hover:bg-gray-600"
-      onClick={handleClick}
-    >
-      <input
-        className="form-check-input cursor-pointer"
-        type="checkbox"
-        value={checked ? '1' : ''}
-        ref={ref}
-        aria-label={title}
-        onChange={onChange}
-      />
-    </span>
-  )
-}
+//   return (
+//     <span
+//       title={title}
+//       className="inline-flex cursor-pointer items-center rounded p-1.5 hover:bg-gray-300 dark:hover:bg-gray-600"
+//       onClick={handleClick}
+//     >
+//       <input
+//         className="form-check-input cursor-pointer"
+//         type="checkbox"
+//         value={checked ? '1' : ''}
+//         ref={ref}
+//         aria-label={title}
+//         onChange={onChange}
+//       />
+//     </span>
+//   )
+// }
 
 export const Downloading: FC<{ title: string; style: string }> = ({ title, style }) => {
   return (
