d5c8cb1ce2ad481de0245682255b43c99f592475
Zkeq <learnonly@learnonly.xyz>
2022-03-30 17:58:21 +0800

    searchengin

--- config/site.config.js
+++ config/site.config.js
@@ -55,12 +55,12 @@ module.exports = {
     {
       "name": "Bilibili",
       "link": "https://space.bilibili.com/36184593",
-      "other": "bilibili"
+      "other": "youtube"
     },
     {
       "name": "搜索",
       "link": "https://www.mubucm.com/doc/71nrYh7YGLN",
-      "other": "magnifying-glass"
+      "other": "searchengin"
     }
   ],
   datetimeFormat: 'YYYY-MM-DD HH:mm:ss'
