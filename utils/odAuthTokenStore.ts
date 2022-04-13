9dbe7b9373e14afdf4b97cac81deec02654f59bf
Zkeq <learnonly@learnonly.xyz>
2022-04-13 16:39:05 +0800

    返回？

--- utils/odAuthTokenStore.ts
+++ utils/odAuthTokenStore.ts
@@ -1,16 +1,12 @@
-var axios = require('axios');
-var qs = require('qs');
-
-const base_url = process.env.BASE_URL
+import Redis from 'ioredis'
 
 // Persistent key-value store is provided by Redis, hosted on Upstash
 // https://vercel.com/integrations/upstash
+const kv = new Redis(process.env.REDIS_URL)
 
 export async function getOdAuthTokens(): Promise<{ accessToken: unknown; refreshToken: unknown }> {
-  const accessToken = await (await axios.get( base_url +'/get')).data
-  console.log("===== get accessToken =====")
-  const refreshToken = await (await axios.get(base_url + '/fresh')).data
-  console.log("===== get refreshToken =====")
+  const accessToken = await kv.get('access_token')
+  const refreshToken = await kv.get('refresh_token')
 
   return {
     accessToken,
@@ -27,46 +23,6 @@ export async function storeOdAuthTokens({
   accessTokenExpiry: number
   refreshToken: string
 }): Promise<void> {
-  // 提交 access_token
-  var access_token_data = qs.stringify({
-    'ak': accessToken,
-    'name': 'access_token' 
-  });
-  var _config = {
-    method: 'post',
-    url: base_url + '/post_ak',
-    headers: { 
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data : access_token_data
-  };
-  axios(_config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-  // 提交 refreshToken
-  var refreshToken_data = qs.stringify({
-    'ak': refreshToken,
-    'name': 'refresh_token' 
-  });
-  var _config = {
-    method: 'post',
-    url: base_url + '/post_ak',
-    headers: { 
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data : refreshToken_data
-  };
-  axios(_config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  await kv.set('access_token', accessToken, 'ex', accessTokenExpiry)
+  await kv.set('refresh_token', refreshToken)
 }
