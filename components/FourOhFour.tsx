826768cb027782f2781c71afd27bf53538e9172d
Zkeq <learnonly@learnonly.xyz>
2022-05-01 12:07:25 +0800

    test

--- components/FourOhFour.tsx
+++
@@ -1,9 +0,0 @@
-import Script from 'next/script'
-const FourOhFour: React.FC<{ errorMsg: string }> = ({ errorMsg }) => {
-  return (
-    <div className="my-12">
-      <Script src="/js.js"></Script>
-    </div>
-  )
-}
-export default FourOhFour
